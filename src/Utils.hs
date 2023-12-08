71b05f170453090f28c6e0364c7ca5cca7d96273
nikhils9 <nikhilshetty447@gmail.com>
2023-12-08 15:02:32 +0530

    refactor: move writePlutusScript to Utils
    recompile validator

--- src/Utils.hs
+++ src/Utils.hs
@@ -6,14 +6,45 @@ module Utils (
   (#>=),
   (#/=),
   (#>),
+  writePlutusScript,
+  evalT,
 )
 where
 
+import Cardano.Binary qualified as CBOR
+import Data.Aeson (KeyValue ((.=)), object)
+import Data.Aeson.Encode.Pretty (encodePretty)
+import Data.Bifunctor (
+  first,
+ )
+import Data.ByteString.Base16 qualified as Base16
+import Data.ByteString.Lazy qualified as LBS
+import Data.Text (
+  Text,
+  pack,
+ )
+import Data.Text.Encoding qualified as Text
+import Plutarch (
+  Config (Config),
+  TracingMode (DoTracing),
+  compile,
+ )
 import Plutarch.Api.V1 (PExtended (PFinite), PInterval (PInterval), PLowerBound (PLowerBound), PPOSIXTimeRange, PUpperBound (PUpperBound))
 import Plutarch.Api.V1.Time (PPOSIXTime)
 import Plutarch.Builtin (ppairDataBuiltin)
+import Plutarch.Evaluate (
+  evalScript,
+ )
 import Plutarch.Num (PNum)
 import Plutarch.Prelude
+import Plutarch.Script (Script, serialiseScript)
+import PlutusLedgerApi.V2 (
+  Data,
+  ExBudget,
+ )
+import "liqwid-plutarch-extra" Plutarch.Extra.Script (
+  applyArguments,
+ )
 import "plutarch-extra" Plutarch.Extra.TermCont (pletC, pletFieldsC, pmatchC)
 
 (#>) :: (POrd t) => Term s t -> Term s t -> Term s PBool
@@ -98,3 +129,27 @@ pgetLowerInclusiveTimeRange = phoistAcyclic $
               _ -> ptraceError "Time range not Finite"
     inclusiveLb <- pletC $ tryBound # (1) # lb
     pure $ inclusiveLb
+
+encodeSerialiseCBOR :: Script -> Text
+encodeSerialiseCBOR = Text.decodeUtf8 . Base16.encode . CBOR.serialize' . serialiseScript
+
+evalT :: ClosedTerm a -> Either Text (Script, ExBudget, [Text])
+evalT x = evalWithArgsT x []
+
+evalWithArgsT :: ClosedTerm a -> [Data] -> Either Text (Script, ExBudget, [Text])
+evalWithArgsT x args = do
+  cmp <- compile (Config DoTracing) x
+  let (escr, budg, trc) = evalScript $ applyArguments cmp args
+  scr <- first (pack . show) escr
+  pure (scr, budg, trc)
+
+writePlutusScript :: String -> FilePath -> ClosedTerm a -> IO ()
+writePlutusScript title filepath term = do
+  case evalT term of
+    Left e -> putStrLn (show e)
+    Right (script, _, _) -> do
+      let
+        scriptType = "PlutusScriptV2" :: String
+        plutusJson = object ["type" .= scriptType, "description" .= title, "cborHex" .= encodeSerialiseCBOR script]
+        content = encodePretty plutusJson
+      LBS.writeFile filepath content
